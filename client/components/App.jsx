f1c0769c00b4ddb1d990e0ba2beb45e511eb045e
Student <student@eda-bc-06.localdomain>
2020-01-31 09:17:01 +1300

    add footer component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 
 const App = () => {
   return (
     <React.Fragment>
     <Header />
+    <div>Some text</div>
+    <Footer/>
     </React.Fragment>
   )
 }
