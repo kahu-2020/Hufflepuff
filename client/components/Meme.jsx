fc88a06d1e0f49265d3cb88463dcbf15273de1df
Scott Thomson <scottthomson_57@hotmail.com>
2020-01-31 14:02:44 +1300

    pseudo-code

--- client/components/Meme.jsx
+++ client/components/Meme.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-
+// created a parent class which sets an ultimate beahviour and a default 'state'
 class Meme extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
+        this.state = { 
+            // this is the behaviour of the class/or state
             topText: '',
             bottomText: '',
             randImg: './random.jpg',
@@ -15,24 +16,30 @@ class Meme extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-
+    // creating a method for getting the data
     componentDidMount() {
+        // using this API to provide images for our meme
         fetch("https://api.imgflip.com/get_memes") 
+        // once the fetch has found the API then convert into a JSON object so we cans read the data
         .then(response => response.json())
         .then(response => {
+            // setting previous JSON object and making it a JS variable called 'memes' that we can then reuse easily
             const { memes } = response.data
+            // we are setting the 'allMemes' state to the response data called 'memes'
             this.setState({ allMemes: memes })
         })
     }
-
+    // this method sets the state of the text box and handles our text input
     handleChange(event) {
+        //create a new variable that targets where to save this information
         const { name, value } = event.target
+        //set the state of our class to the target variable.
         this.setState({ [name]: value })
     }
 
+    
     handleSubmit(e){
         e.preventDefault()
-
         const rand = Math.floor(Math.random() * this.state.allMemes.length)
         const randMeme = this.state.allMemes[rand].url
         console.log(randMeme)
